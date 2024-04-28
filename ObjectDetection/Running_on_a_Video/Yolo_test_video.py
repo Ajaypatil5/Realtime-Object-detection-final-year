4ef2eeb50581b151aec5a4586f573715bcf2ab81
Ajaypatil5 <amanpatil19211@gmail.com>
2024-04-28 20:18:56 +0530

    First Commit

---
+++ ObjectDetection/Running_on_a_Video/Yolo_test_video.py
@@ -0,0 +1,62 @@
+from ultralytics import YOLO
+import cv2
+import math
+import os
+
+# Check if the output directory exists, if not, create it
+output_dir = 'output_videos'
+os.makedirs(output_dir, exist_ok=True)
+
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('../VIDEOS/highway_traffic.mp4')
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+out_filename = os.path.join(output_dir, 'output.avi')
+out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc(*'XVID'), 30, (frame_width, frame_height))
+
+model = YOLO("../YOLO-Weights/yolov8n.pt")
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"]
+
+while True:
+    success, img = cap.read()
+
+    if not success:
+        print("Error: Failed to read frame from the video.")
+        break
+
+    results = model(img, stream=True)
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            cls = int(box.cls[0])
+            class_name = classNames[cls]
+            label = f'{class_name}{conf}'
+            t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=1)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
+            cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
+            cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+
+    out.write(img)
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+out.release()
+cap.release()
+cv2.destroyAllWindows()
